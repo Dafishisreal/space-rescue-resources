dcbc8381e31b01c16e64b5ed332fc7f62c5f81bf
Dafishisreal <89615868+Dafishisreal@users.noreply.github.com>
2023-07-27 12:04:34 +1000

    Ship

---
+++ Objects/Ship.py
@@ -0,0 +1,16 @@
+from GameFrame import Level, RoomObject
+import pygame
+
+class Ship(RoomObject):
+
+    def __init__(self,room,x,y):
+        RoomObject.__init__(self,room,x,y)
+        image = self.load_image("Ship.png")
+        self.set_image(image,100,100)
+        self.handle_key_events = True
+    def key_pressed(self, key):
+        if key[pygame.k_w]:
+            self.y_speed = -10
+        elif key[pygame.K_s]:
+            self.y_speed = 10
+       
