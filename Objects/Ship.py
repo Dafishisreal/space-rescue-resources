34187a12cf18480a3d4f75230ebe557723976bfb
Dafishisreal <89615868+Dafishisreal@users.noreply.github.com>
2023-07-31 13:49:20 +1000

    changes

--- Objects/Ship.py
+++ Objects/Ship.py
@@ -7,18 +7,25 @@ class Ship(RoomObject):
     def __init__(self,room,x,y):
         RoomObject.__init__(self,room,x,y)
         image = self.load_image("Ship.png")
-        self.set_image(image,100,100)
         self.handle_key_events = True
     def key_pressed(self, key):
         if key[pygame.K_w]:
-            self.y -= 10
+            self.y -= 15
         elif key[pygame.K_s]:
-            self.y += 10
+            self.y += 15
+        elif key[pygame.K_a]:
+            self.x -= 15
+        elif key[pygame.K_d]:
+            self.x += 15
     def keep_in_room(self):
         if self.y < 0: 
             self.y = 0 
         elif self.y + self.height > Globals.SCREEN_HEIGHT:
             self.y = Globals.SCREEN_HEIGHT - self.height
+        elif self.x < 0: 
+            self.x = 0 
+        elif self.x + self.width > Globals.SCREEN_WIDTH:
+            self.x = Globals.SCREEN_WIDTH - self.width
     def step(self):
         self.keep_in_room()
        
